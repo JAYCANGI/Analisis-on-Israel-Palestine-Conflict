d40cdaf1cd223f6f9a3daecfc5bc8d67c9ba8a49
JAYCANGI <jayshankarcangi@gmail.com>
2023-12-06 16:26:50 +0100

    Update de los archivos. Los datos se cargan bien y tenemos output en la aplicacion de streamlit

--- fastapi/server.py
+++ fastapi/server.py
@@ -5,7 +5,10 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
+from datetime import datetime
+import logging
 
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -35,6 +38,13 @@ class Item(Base):
     killed_by = Column(String)
     notes = Column(Text)
 
+    def __init__(self, **data):
+        print(data)  # Add this line to print the data
+        valid_data = {k: v for k, v in data.items() if k not in {'Unnamed: 16'}}
+        super().__init__(**valid_data)
+
+    
+
 # Set up a connection to the database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -44,14 +54,41 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def read_root():
     return {"message": "Welcome to the FastAPI application!"}
 
-# FastAPI route to load data from CSV into the database
 
+# FastAPI route to load data from CSV into the database
+# FastAPI route to load data from CSV into the database
 @app.get("/load-data")
 def load_data():
     try:
-        # Load data from CSV
+        # Load data from CSV excluding any unnamed columns
         csv_path = "data.csv"
-        df = pd.read_csv(csv_path)
+        df = pd.read_csv(csv_path, index_col=0)  # Assuming the index is in the first column
+        df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+        # Rename columns to match the Item model attributes
+        df = df.rename(columns={
+            "Name": "name",
+            "Date of Event": "date_of_event",
+            "Age": "age",
+            "Citizenship": "citizenship",
+            "Event Location": "event_location",
+            "Event Location District": "event_location_district",
+            "Event Location Region": "event_location_region",
+            "Date of Death": "date_of_death",
+            "Gender": "gender",
+            "Took Part in the Hostilities": "took_part_in_the_hostilities",
+            "Place of Residence": "place_of_residence",
+            "Place of Residence District": "place_of_residence_district",
+            "Type of Injury": "type_of_injury",
+            "Ammunition": "ammunition",
+            "Killed By": "killed_by",
+            "Notes": "notes"
+        })
+
+        # Convert date columns to datetime objects
+        date_columns = ["date_of_event", "date_of_death"]
+        for col in date_columns:
+            df[col] = pd.to_datetime(df[col], errors='coerce')
 
         # Print CSV file information
         print("CSV file loaded successfully.")
@@ -66,9 +103,12 @@ def load_data():
         db = SessionLocal()
 
         # Insert data into the database
-        for _, row in df.iterrows():
-            item = Item(**row.to_dict())
-            db.add(item)
+        for idx, row in df.iterrows():
+            try:
+                item = Item(**row.to_dict())
+                db.add(item)
+            except Exception as e:
+                print(f"Error inserting data at index {idx}: {str(e)}")
 
         # Commit the changes
         db.commit()
@@ -79,9 +119,10 @@ def load_data():
         db.close()
 
         return {"status": "Data loaded successfully!"}
-
+        
     except Exception as e:
         return {"status": "Error", "error_message": str(e)}
+
 
 # FastAPI health check endpoint
 @app.get("/health")
@@ -96,6 +137,7 @@ def health_check():
         return {"status": "Error", "error_message": str(e)}
 
 # FastAPI route to serve the dataset
+# FastAPI route to serve the dataset
 @app.get("/items")
 def read_data():
     # Fetch data from the database using SQLAlchemy
@@ -103,14 +145,29 @@ def read_data():
     data = db.query(Item).all()
     db.close()
 
-    # Convert data to JSON
-    response_data = [{"id": item.id, "name": item.name, "date_of_event": item.date_of_event,
-                     "age": item.age, "citizenship": item.citizenship, "event_location": item.event_location,
-                     "event_location_district": item.event_location_district, "event_location_region": item.event_location_region,
-                     "date_of_death": item.date_of_death, "gender": item.gender,
-                     "took_part_in_the_hostilities": item.took_part_in_the_hostilities,
-                     "place_of_residence": item.place_of_residence, "place_of_residence_district": item.place_of_residence_district,
-                     "type_of_injury": item.type_of_injury, "ammunition": item.ammunition,
-                     "killed_by": item.killed_by, "notes": item.notes} for item in data]
+    # Convert data to JSON, converting datetime objects to strings
+    response_data = [
+        {
+            "id": item.id,
+            "name": item.name,
+            "date_of_event": item.date_of_event.strftime("%Y-%m-%dT%H:%M:%S") if item.date_of_event else None,
+            "age": item.age,
+            "citizenship": item.citizenship,
+            "event_location": item.event_location,
+            "event_location_district": item.event_location_district,
+            "event_location_region": item.event_location_region,
+            "date_of_death": item.date_of_death.strftime("%Y-%m-%dT%H:%M:%S") if item.date_of_death else None,
+            "gender": item.gender,
+            "took_part_in_the_hostilities": item.took_part_in_the_hostilities,
+            "place_of_residence": item.place_of_residence,
+            "place_of_residence_district": item.place_of_residence_district,
+            "type_of_injury": item.type_of_injury,
+            "ammunition": item.ammunition,
+            "killed_by": item.killed_by,
+            "notes": item.notes,
+        }
+        for item in data
+    ]
 
     return JSONResponse(content=response_data)
+
